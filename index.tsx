59e38508ca10bd6af0411ed9900f6976d3e55e5f
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2025-06-18 23:51:09 +0700

    show list of users

--- index.tsx
+++ index.tsx
@@ -1,4 +1,7 @@
 import { render, Box, Text, useApp, useInput, useStdout } from "ink";
+import { useEffect, useState } from "react";
+import { initializeApp, type App } from "firebase-admin/app";
+import { getAuth, UserRecord } from "firebase-admin/auth";
 
 const ENTER_ALTERNATE_SCREEN = "\x1b[?1049h";
 const EXIT_ALTERNATE_SCREEN = "\x1b[?1049l";
@@ -6,6 +9,19 @@ const EXIT_ALTERNATE_SCREEN = "\x1b[?1049l";
 const App = () => {
   const { exit } = useApp();
   const { stdout } = useStdout();
+
+  const [users, setUsers] = useState<UserRecord[]>([]);
+
+  // Initialize Firebase Admin SDK
+  useEffect(() => {
+    const app = initializeApp();
+
+    getAuth(app)
+      .listUsers(5)
+      .then((result) => {
+        setUsers(result.users);
+      });
+  }, []);
 
   // Listen for keypresses
   useInput((input, key) => {
@@ -21,17 +37,24 @@ const App = () => {
       width={stdout.columns}
       height={stdout.rows}
     >
-      <Text>
-        Press{" "}
-        <Text bold color="red">
-          "q"{" "}
+      <Box flexDirection="column">
+        <Text>
+          Press{" "}
+          <Text bold color="red">
+            "q"{" "}
+          </Text>
+          or{" "}
+          <Text bold color="red">
+            ctrl+C{" "}
+          </Text>
+          to exit.
         </Text>
-        or{" "}
-        <Text bold color="red">
-          ctrl+C{" "}
-        </Text>
-        to exit.
-      </Text>
+        <Box marginTop={1} flexDirection="column">
+          {users.map((user) => (
+            <Text key={user.uid}>{user.uid}</Text>
+          ))}
+        </Box>
+      </Box>
     </Box>
   );
 };
