9026bdb6583e04a1fe49a74f9cb5dce1cd40ecdc
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2025-06-19 23:25:32 +0700

    update layout

--- index.tsx
+++ index.tsx
@@ -1,8 +1,9 @@
-import { Box, Text, useApp, useInput } from "ink";
+import { Box, Spacer, Text, useApp, useInput } from "ink";
 import { useEffect, useState } from "react";
 import { initializeApp, type App } from "firebase-admin/app";
 import { getAuth, UserRecord } from "firebase-admin/auth";
 import { withFullScreen } from "fullscreen-ink";
+import { format } from "date-fns";
 
 const App = () => {
   const { exit } = useApp();
@@ -31,30 +32,59 @@ const App = () => {
     }
   });
 
+  function formatDate(date: string | null | undefined) {
+    if (!date) return "-";
+
+    return format(new Date(date), "d MMM, HH:mm");
+  }
+
   return (
-    <Box justifyContent="center" alignItems="center" width="100%" height="100%">
-      <Box flexDirection="column">
-        <Text>
-          Press{" "}
-          <Text bold color="red">
-            "q"{" "}
+    <Box padding={1} paddingBottom={0}>
+      <Box flexDirection="column" width="100%">
+        <Box paddingLeft={2}>
+          <Text color="magenta" bold>
+            Arrogance Admin
           </Text>
-          or{" "}
-          <Text bold color="red">
-            ctrl+C{" "}
-          </Text>
-          to exit.
-        </Text>
+        </Box>
+        <Box paddingLeft={2} marginTop={1}>
+          <Text bold>Users</Text>
+        </Box>
         <Box marginTop={1} flexDirection="column">
-          {users.map((user, index) => (
-            <Text
-              key={user.uid}
-              backgroundColor={index === cursor ? "magenta" : ""}
-              color={index === cursor ? "#FFF" : ""}
-            >
-              {user.uid}
+          {users.length ? (
+            users.map((user, index) => (
+              <Box key={user.uid} flexDirection="column" marginBottom={1}>
+                <Text color={index === cursor ? "magenta" : ""} bold>
+                  {index === cursor ? ">" : " "} {user.uid}
+                </Text>
+                <Box paddingLeft={2} flexDirection="column">
+                  <Text>
+                    Created at: {formatDate(user.metadata.creationTime)}
+                  </Text>
+                  <Text>
+                    Last login: {formatDate(user.metadata.lastRefreshTime)}
+                  </Text>
+                </Box>
+              </Box>
+            ))
+          ) : (
+            <Box paddingLeft={2}>
+              <Text italic>Loading...</Text>
+            </Box>
+          )}
+        </Box>
+        <Spacer />
+        <Box paddingLeft={2}>
+          <Text>
+            Press{" "}
+            <Text bold color="red">
+              "q"{" "}
             </Text>
-          ))}
+            or{" "}
+            <Text bold color="red">
+              ctrl+C{" "}
+            </Text>
+            to exit.
+          </Text>
         </Box>
       </Box>
     </Box>
